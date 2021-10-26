9fe55b3383e4a0ed80029ea17dee38146902509e
Willin Wang <willin@willin.org>
2021-10-26 14:33:40 +0800

    feat: :sparkles: init the project with rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'es'
+      }
+    ],
+    plugins: [typescript({ declaration: true, declarationDir: 'dist' })]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs.js',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/index.browser.js',
+        format: 'iife',
+        name: 'uua'
+      }
+    ],
+    plugins: [typescript()]
+  }
+];
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export interface AbortableFetch {
+  about: () => void;
+  ready: Promise<Response>;
+}
+
+export function abortableFetch(request: RequestInfo, opts: RequestInit = {}): AbortableFetch {
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  return {
+    abort: (): void => controller.abort(),
+    ready: fetch(request, { ...opts, signal })
+  };
+}
