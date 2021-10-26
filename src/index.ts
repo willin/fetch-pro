bbe47a7b5ffd56e4619095211c86a1d19c960427
Willin Wang <willin@willin.org>
2021-10-26 14:48:08 +0800

    feat: :sparkles: add FetchPro class

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,44 @@ export function abortableFetch(request: RequestInfo, opts: RequestInit = {}): Ab
     ready: fetch(request, { ...opts, signal })
   };
 }
+
+export enum FetchType {
+  CancelAndResend = 'CancelAndResend',
+  Prevent = 'Prevent'
+}
+
+export class FetchPro {
+  private client: AbortableFetch | null = null;
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(public readonly type: FetchType) {}
+
+  public abort(): void {
+    if (this.client) {
+      this.client.abort();
+      this.client = null;
+    }
+  }
+
+  public fetch(request: RequestInfo, opts: RequestInit = {}): Promise<Response> {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    if (this.client) {
+      if (this.type === FetchType.Prevent) {
+        return this.client.ready;
+      }
+      this.client.abort();
+      this.client = null;
+    }
+
+    this.client = {
+      abort: (): void => controller.abort(),
+      ready: fetch(request, { ...opts, signal }).then((res: Response) => {
+        this.client = null;
+        return res;
+      })
+    };
+    return this.client.ready;
+  }
+}
